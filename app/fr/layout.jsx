5bd0826e8d272c54acfd7d9b73dee85214454c25
AscendantAutoDetailing <yasshac01@gmail.com>
2026-04-05 23:11:54 -0400

    Update layout.jsx

--- app/fr/layout.jsx
+++ app/fr/layout.jsx
@@ -1,10 +1,3 @@
-import Header from "../../components/Header";
-
 export default function Layout({ children }) {
-  return (
-    <div>
-      <Header lang="fr" />
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
