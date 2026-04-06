256cadc68aef87b5be6964fe7e5727bb0536af98
AscendantAutoDetailing <yasshac01@gmail.com>
2026-04-05 23:02:18 -0400

    Update layout.jsx

--- app/en/layout.jsx
+++ app/en/layout.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header";
 
-export default function EnglishLayout({ children }) {
+export default function Layout({ children }) {
   return (
     <div>
       <Header lang="en" />
