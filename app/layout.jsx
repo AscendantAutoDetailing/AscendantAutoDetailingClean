5ecac10771a4c232667a0282202c4630f5b7b6a8
AscendantAutoDetailing <yasshac01@gmail.com>
2026-04-08 00:54:11 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body style={{ margin: 0 }}>
+      <body>
         {children}
       </body>
     </html>
